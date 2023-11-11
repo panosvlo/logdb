3bf989a4d5581d2d0c2a27f409c79f1052379888
panagiotis.vlontzos <panagiotis.vlontzos@sword-group.com>
2023-11-11 23:45:03 +0200

    Fist commit, springboot initialized

---
+++ src/main/java/gr/uoa/di/cs/logdb/LogdbApplication.java
@@ -0,0 +1,13 @@
+package gr.uoa.di.cs.logdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/uoa/di/cs/logdb/LogdbApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.uoa.di.cs.logdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
