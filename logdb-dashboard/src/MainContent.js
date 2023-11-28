b3fcf8eed886897ed5e551a30726d64c27967f55
panagiotis.vlontzos <panagiotis.vlontzos@sword-group.com>
2023-11-28 06:24:41 +0200

    Fix the displaying of the 1st query

--- logdb-dashboard/src/MainContent.js
+++ logdb-dashboard/src/MainContent.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+const logTypeDescriptions = {
+  '1': 'access_log',
+  '3': 'HDFS_FS_Namesystem',
+  '2': 'HDFS_DataXceiver'
+};
+
 const MainContent = ({ data }) => {
   // Handle case where data is not an array
   if (!data) return null;
@@ -13,14 +19,16 @@ const MainContent = ({ data }) => {
       <table>
         <thead>
           <tr>
-            {keys.map(key => <th key={key}>{key}</th>)}
+            {keys.map(key => <th key={key}>{key === 'logTypeId' ? 'Log Type' : key}</th>)}
           </tr>
         </thead>
         <tbody>
           {dataArray.map((item, index) => (
             <tr key={index}>
               {keys.map(key => (
-                <td key={key}>{item[key]}</td>
+                <td key={key}>
+                  {key === 'logTypeId' ? logTypeDescriptions[item[key].toString()] : item[key]}
+                 </td>
               ))}
             </tr>
           ))}
