cbdf23f8c98b3e8690cad83babdc248732884f2d
panagiotis.vlontzos <panagiotis.vlontzos@sword-group.com>
2023-11-28 04:15:46 +0200

    Implemented all APIs in frontend, API 10 needs fixing

--- logdb-dashboard/src/apiConfig.js
+++ logdb-dashboard/src/apiConfig.js
@@ -45,6 +45,54 @@ const apis = [
       { name: 'size', type: 'number', placeholder: 'Size', value: '300' } // Default value set to 300
     ]
   },
+  {
+      name: 'API 7',
+      endpoint: '/api/logs/accessLogs/firefox',
+      params: [] // No parameters required for this API
+    },
+    {
+      name: 'API 8',
+      endpoint: '/api/logs/blocks',
+      params: [] // No parameters required for this API
+    },
+    {
+      name: 'API 9',
+      endpoint: '/api/logs/blockAllocationsAndReplicationsSameHour',
+      params: [] // No parameters required for this API
+    },
+    {
+        name: 'API 10',
+        endpoint: '/api/logs/methodUsage',
+        params: [
+          { name: 'httpMethod', type: 'text', placeholder: 'HTTP Method', value: 'GET' },
+          { name: 'startDate', type: 'datetime-local', placeholder: 'Start Date', value: '' },
+          { name: 'endDate', type: 'datetime-local', placeholder: 'End Date', value: '' }
+        ]
+      },
+      {
+        name: 'API 11',
+        endpoint: '/api/logs/ipsWithTwoMethods',
+        params: [
+          { name: 'method1', type: 'text', placeholder: 'Method 1', value: 'GET' },
+          { name: 'method2', type: 'text', placeholder: 'Method 2', value: 'POST' },
+          { name: 'startDate', type: 'date', placeholder: 'Start Date', value: '' },
+          { name: 'endDate', type: 'date', placeholder: 'End Date', value: '' }
+        ]
+      },
+      {
+        name: 'API 12',
+        endpoint: '/api/logs/distinctMethods',
+        params: [
+          { name: 'startDate', type: 'date', placeholder: 'Start Date', value: '' },
+          { name: 'endDate', type: 'date', placeholder: 'End Date', value: '' },
+          { name: 'minMethods', type: 'number', placeholder: 'Minimum Methods', value: 2 }
+        ]
+      },
+      {
+        name: 'API 13',
+        endpoint: '/api/logs/referrers/multiple-resources',
+        params: [] // No parameters required for this API
+      },
 ];
 
 export default apis;
